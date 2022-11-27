99e85abd86dc44386e92567d1326b90a75784dfb
Ivan Lazariev <lazariev.ivan02@gmail.com>
2022-11-27 21:38:37 +0200

    Changed layout, fixed image sizing, implemented changing skills in list

--- src/application/BuilderController.java
+++ src/application/BuilderController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -44,7 +45,7 @@ public class BuilderController {
         fileChooser.getExtensionFilters().add(allowedExtensions);
         fileChooser.setTitle("Select profile picture");
         File imageFile = fileChooser.showOpenDialog(this.stage);
-        Image image = new Image(imageFile.toURI().toString());
+        Image image = new Image(imageFile.toURI().toString(), profileImage.getFitWidth(), profileImage.getFitHeight(), false, true);
         if (image != null) {
             profileImage.setImage(image);
         }
@@ -55,10 +56,12 @@ public class BuilderController {
     }
 
     public void addSkill(ActionEvent e) {
-        TextField skill = this.inputHandler.getInput("#addSkillInput", getScene());
+        TextField skill = this.inputHandler.getInput("#addSkillInput", getScene());    
         String skillText = skill.getText();
+        skill.setText("");
         Node listNode = this.getScene().lookup("#skillsList");
         ListView<String> skillsList = ((ListView<String>)listNode);
+        skillsList.setCellFactory(TextFieldListCell.forListView());
         skillsList.getItems().add(skillText);
     }
 
